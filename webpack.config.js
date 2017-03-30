0f40738c816c1bf1d8ffdad48b0c8b113d3a51e3
mike_gichia <mqyynm@gmail.com>
2017-03-30 13:35:30 +0300

    Refactoring and tooling.

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
 				test: /\.js?$/,
 				exclude: /(node_modules)/,
 				query: {
-					presets: ['react', 'es2015']
+					presets: ["es2015", "react", "stage-0"]
 				}
 			}
 		]
