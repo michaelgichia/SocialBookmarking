d495640564aca165ed7b9fb0f780e44e0256d592
mike_gichia <mqyynm@gmail.com>
2017-03-31 11:50:59 +0300

    :zap: Return profile after registration.

--- src/utils/APIManager.js
+++ src/utils/APIManager.js
@@ -25,6 +25,11 @@ export default {
 				callback(err, null)
 				return
 			}
+			const confirmation = response.body.confirmation
+			if (confirmation != 'success'){
+				callback({message: response.body.message}, null)
+				return
+			}			
 			callback(null, response.body)
 		})
 	}
